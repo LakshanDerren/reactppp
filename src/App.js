34f5c7470f53216df834410af4efd6a4989a5ea4
LakshanDerren <145681127+LakshanDerren@users.noreply.github.com>
2024-08-19 17:55:50 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
 import logo from './logo.svg';
+import { useEffect, useState } from "react"
+import axios from "axios"
 import './App.css';
 
 function App() {
+  const [msg , setmsg] = useState([])
+  const [Ivalue , setIvalue] = useState()
+ 
+  // function addmsg(Ivalue){
+  //   let list1 = []
+  //   list1 = [...msg,Ivalue]
+  //   setmsg(list1)
+  //   console.log(list1);
+    
+  // }
+  useEffect(()=>{
+    getIvalue()
+  },[])
+    function getIvalue() {
+      let list = []
+      axios.get("http://localhost:3000/get")
+        .then(res=>{
+          console.log(res);
+          for (let index = 0; index < res.data.length; index++) {
+            const element = res.data[index].msg;
+            list = [...list,element]
+          setmsg(list)
+          } 
+        })
+        .catch(err=>{
+          console.log("axios err");
+        })
+    }
+    
+    function addIvalue(Ivalue) {
+      axios.post("http://localhost:3000/post",{msg:Ivalue})
+      .then(()=>{
+        console.log("worked");
+        getIvalue()
+      })
+    }
+
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <div className="body">
+        <div className="midBody">
+        <nav>
+          <div className="logo">PIGGYROOM</div>
+          <div className="text">
+            <a href="">log in</a>
+            <a href="">Register</a>
+          </div>
+        </nav>
+        <div className="msgarea">
+          
+    {msg.map(function(object, i){
+        return <p key={i}>{object}</p>;
+    })}
+          
+        </div>
+        <div className="downarea">
+            <input type="text" value={Ivalue} placeholder="say somthin'" onChange={e=>{setIvalue(e.target.value)}}/>
+            <button onClick={()=>{addIvalue(Ivalue)
+              setIvalue("")
+            }}>send</button>
+        </div>
+        </div>
+      </div>
+    </>
+  )
 }
 
 export default App;
